189dcfd9fd7b7bf568056945eefb52e43f0af928
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-14 09:37:47 +0100

    created TOP collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      # / restaurants/...
+      get 'top', to: 'restaurants#top'
+    end
+  end
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
